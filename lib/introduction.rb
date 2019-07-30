461b39658f8a18c2d4f3c6a40e2e8733024100e2
sahduddin ahmed <sahmed395@gmail.com>
2019-07-30 14:21:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 def method(name)
      puts "Hi, My Name is #{name}."
@@ -5,3 +6,12 @@ end
 
 method("Saad")
 
+=======
+
+def method(name)
+     puts "Hi, My Name is #{name}."
+end
+
+method("Saad")
+
+>>>>>>> 8a4562ca9d8964a0e03ea7fd725ce2efbb9cbc9b
